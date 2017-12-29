8f55d8a9d94286214d42945f5b773f566016bf43
Paolo <paolo@frontgroupdesign.com>
2017-12-29 01:51:01 -0800

    Install Reactstrap and Bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -10,9 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to DeckDuck</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button color="primary">Create New Deck</Button>
       </div>
     );
   }
